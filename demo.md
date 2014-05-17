1e49fe102d548e5ceb7352e9c2121048f811e0e1
Andre Rodrigues <andrerod@microsoft.com>
2014-05-17 23:30:09 +0100

    Add demo URL

--- demo.md
+++ demo.md
@@ -5,4 +5,9 @@ Mostrar azure cli
   - Fazer deploy do site: azure site create --git / git push azure
   - Ver os deployments: azure deployment list
   - Abrir o site: azure site browse
-  
+Mostrar o SDK
+  - Ler as linhas de codigo
+  - Mostrar os backgrounds
+  - Fazer upload de uma imagem alternativa
+  - Ver que agora usa a que esta no blob
+Kinnect play
--- demo/fruity-server/public/src/index.js
+++ demo/fruity-server/public/src/index.js
@@ -1,41 +1,78 @@
 (function() {
   if (!Detector.webgl) {
     Detector.addGetWebGLMessage();
-  }
+  } else {
+    function displayError(message) {
+      // create unique id for div
+      var dialog_id = "error_dialog_" + new Date().getTime();
 
-  $("#username-dialog").dialog({
-    modal: true,
-    resizable: false,
-    buttons: {
-      "Ok": function() {
-        $.get('/get_username_score?username=' + $('#username').val(), function (data) {
-          $('#max_score').text(data.max_score);
-          $('#username-dialog').dialog("close");
+      // append html to body
+      $("body").append("<div id=\"" + dialog_id + "\">" + message + "</div>");
 
-          initGame();
-        });
+      // display the error dialog
+      $("#" + dialog_id).dialog({
+        modal: true,
+        resizable: false,
+        height: 175,
+        width: 400,
+        title: "Oops, something went wrong!",
+        buttons: {
+          Ok: function() {
+            $(this).dialog("close");
+
+            initGame();
+          }
+        },
+        beforeClose: function(event, ui) {
+          // remove dialog div from document
+          $("#" + dialog_id).remove();
+        }
+      });
+    }
+
+    function initGame() {
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+      var aspect = 16 / 9;
+      if (width / aspect > height) {
+        width = height * aspect;
+      } else {
+        height = width / aspect;
       }
+
+      var opts = {
+        width: width,
+        height: height,
+        container: $('#container')
+      };
+
+      var game = new Game(opts);
+
+      game.initScene(function () {
+        game.initKinectController();
+
+        game.renderLoop();
+      });
     }
-  });
 
-  function initGame() {
-    var width = window.innerWidth;
-    var height = window.innerHeight;
-    var aspect = 16 / 9;
-    if (width / aspect > height) {
-      width = height * aspect;
-    } else {
-      height = width / aspect;
-    }
+    // Start game by prompting for user
+    $("#username-dialog").dialog({
+      modal: true,
+      resizable: false,
+      buttons: {
+        "Ok": function() {
+          $.get('/get_username_score?username=' + $('#username').val(), function (data) {
+            $('#max_score').text(data.max_score);
+            $('#username-dialog').dialog("close");
 
-    var opts = {
-      width: width,
-      height: height,
-      container: document.getElementById('container'),
-    };
-
-    var game = new Game(opts);
-    game.initScene();
-    game.renderLoop();
+            if (data.error) {
+              displayError(data.error);
+            } else {
+              initGame();
+            }
+          });
+        }
+      }
+    });
   }
 })();
