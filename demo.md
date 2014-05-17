1f4cc7bf58a458aef8dc5a616c56f15858eec292
Andre Rodrigues <andrerod@microsoft.com>
2014-05-17 22:07:55 +0100

    Update

---
+++ demo.md
@@ -0,0 +1,8 @@
+Mostrar azure cli
+  - Login: azure site login
+  - Escolher subscricao: azure account list / azure account set
+  - Fazer list de sites: azure site list
+  - Fazer deploy do site: azure site create --git / git push azure
+  - Ver os deployments: azure deployment list
+  - Abrir o site: azure site browse
+  
--- demo/fruity-server/server.js
+++ demo/fruity-server/server.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 var azure = require('azure');
 
 var app = express();
 
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/public'));
 
@@ -11,36 +13,48 @@ app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
-app.use(express.bodyParser());
-
 app.get('/', function(req, res){
   res.render('index');
 });
 
 app.get('/background', function (req, res) {
-  var blobService = azure.createBlobService();
-  blobService.listBlobs('azure', function (err, blobs) {
-    if (!err && blobs.length > 0) {
-      blobService.getBlob('azure', blobs[0].name).pipe(res);
-    } else {
-      res.status(404).send('Not found');
-    }
-  });
+  function sendLocal() {
+    // use local copy
+    console.log('sending local file');
+    fs.createReadStream('./public/images/background.jpg').pipe(res);
+  }
+
+  try {
+    var blobService = azure.createBlobService();
+    blobService.listBlobs('azure', function (err, blobs) {
+      if (!err && blobs.length > 0) {
+        blobService.getBlob('azure', blobs[0].name).pipe(res);
+      } else {
+        sendLocal();
+      }
+    });
+  } catch (e) {
+    sendLocal();
+  }
 });
 
 app.get('/get_username_score', function (req, res) {
   var username = req.query.username;
 
-  var tableService = azure.createTableService();
-  tableService.createTableIfNotExists('users', function () {
-    tableService.queryEntity ('users', 'user', username, function (err, user) {
-      if (!err && user) {
-        res.json({ max_score: user.max_score });
-      } else {
-        res.json({ max_score: 0 });
-      }
+  try {
+    var tableService = azure.createTableService();
+    tableService.createTableIfNotExists('users', function () {
+      tableService.queryEntity ('users', 'user', username, function (err, user) {
+        if (!err && user) {
+          res.json({ max_score: user.max_score });
+        } else {
+          res.json({ max_score: 0 });
+        }
+      });
     });
-  });
+  } catch (e) {
+    res.json({ max_score: 0 });
+  }
 });
 
 app.post('/update_username_score', function (req, res) {
@@ -65,7 +79,5 @@ app.post('/update_username_score', function (req, res) {
   });
 });
 
-if (!module.parent) {
-  app.listen(process.env.PORT || 8080);
-  console.log('Express app started on port 8080');
-}
+app.listen(process.env.PORT || 8080);
+console.log('Express app started on port 8080');
