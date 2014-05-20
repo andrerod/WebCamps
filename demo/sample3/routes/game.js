a25fd5b1849d89579a87d7a57bf1ae5ba61ab75c
Andre Rodrigues <andrerod@microsoft.com>
2014-05-20 15:20:50 +0100

    Add sample 3

---
+++ demo/sample3/routes/game.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var azure = require('azure');
+
+/* GET users listing. */
+router.get('/background', function(req, res) {
+  try {
+    var blobService = azure.createBlobService();
+    blobService.listBlobs('azure', function (err, blobs) {
+      if (!err && blobs.length > 0) {
+        blobService.getBlob('azure', blobs[0].name).pipe(res);
+      } else {
+		res.status(404).send('Not found');
+      }
+    });
+  } catch (e) {
+	res.status(404).send('Not found');
+  }
+});
+
+module.exports = router;
